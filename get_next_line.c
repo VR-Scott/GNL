f8d4441233320f6a360f9eb7efecc0c492cd6b2f
Vaughan SCOTT <vscott@c4r8s8.wethinkcode.co.za>
2019-06-08 16:06:18 +0200

    committing 2nd

--- get_next_line.c
+++ get_next_line.c
@@ -6,14 +6,47 @@
 /*   By: vscott <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/08 13:46:10 by vscott            #+#    #+#             */
-/*   Updated: 2019/06/08 14:13:48 by vscott           ###   ########.fr       */
+/*   Updated: 2019/06/08 16:05:32 by vscott           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include "libft/libft.h"
 
-int	get_next_line(const int fd, char **line)
+static char	ft_findfd(t_list **begin, int fd)
+{
+	t_list *tmp;
+
+	tmp = *begin;
+	if(tmp)
+	{
+		while (tmp)
+		{
+			if (fd == (int)tmp->content_size)
+				return (tmp);
+			tmp = tmp->next;
+		}
+		tmp = ft_lstnew("\0", 1);
+		tmp->content_size = fd;
+		ft_lstadd(begin, tmp);
+		return (tmp);
+	}
+}
+
+static char *ft_freejoin_lst(char *tmp, char *buf)
+{
+	size_t	len;
+	char	*fr;
+	
+	if (!tmp || !buf)
+		return (NULL);
+	free = tmp;
+	tmp = ft_strjoin(tmp, buf, ret);
+	free(fr);
+	return (tmp);
+}
+
+int			get_next_line(const int fd, char **line)
 {
 	int				ret;
 	char			buf[BUFF_SIZE + 1];
