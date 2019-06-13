ca0c2d4d6b9257f7ef26b2db9245c1e785b5ea4d
Vaughan SCOTT <vscott@c4r8s6.wethinkcode.co.za>
2019-06-13 17:40:05 +0200

    commiting stdinmain

--- test/gnlstdin_main.c
+++ test/gnlstdin_main.c
@@ -2,12 +2,30 @@
 #include "../get_next_line.h"
 #include <fcntl.h>
 #include <stdlib.h>
+#include "../libft/libft.h"
 
-int	main(void)
+int	main(int argc, char **argv)
 {
 	char *line;
+	int *fds;
+	int i;
 
-	printf("GNL returned :%i\n", get_next_line(0, &line));
-	printf("line is: %s\n", line);
+	if (!(fds = (int*)malloc(sizeof(int) * argc)))
+		return (-1);
+	i = 0;
+	while (i <= argc)
+	{
+		fds[i] = open(argv[i + 1], O_RDONLY);
+		printf("%d : %s\n", i, argv[i + 1]);
+	}
+	printf("-1 : Exit\n");
+	
+	while (get_next_line(0, &line) > 0 && (i = ft_atoi(line)) > - 1 && i <= argc)
+	{
+		get_next_line(fds[i], &line);
+		printf("%s:\n%s\n",argv[i + 1], line);
+	}
+	free(fds);
+	free(line);
 	return (0);
 }
