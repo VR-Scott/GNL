1f8371969e54714ca312878848fdd3b90ff07549
Vaughan SCOTT <vscott@c4r8s8.wethinkcode.co.za>
2019-06-14 07:26:35 +0200

    gnlstdin_main can print line from chosen file

--- test/gnlstdin_main.c
+++ test/gnlstdin_main.c
@@ -9,21 +9,36 @@ int	main(int argc, char **argv)
 	char *line;
 	int *fds;
 	int i;
+	int fi;
 
 	if (!(fds = (int*)malloc(sizeof(int) * argc)))
 		return (-1);
 	i = 0;
-	while (i <= argc)
+	while (i < argc - 1)
 	{
 		fds[i] = open(argv[i + 1], O_RDONLY);
-		printf("%d : %s\n", i, argv[i + 1]);
+		printf("%d\t: %s\n", i, argv[i + 1]);
+		i++;
 	}
-	printf("-1 : Exit\n");
-	
+	printf("-1\t: Exit\n");
 	while (get_next_line(0, &line) > 0 && (i = ft_atoi(line)) > - 1 && i <= argc)
 	{
 		get_next_line(fds[i], &line);
 		printf("%s:\n%s\n",argv[i + 1], line);
+		fi = 0;
+		while (fi < argc - 1)
+		{
+			printf("%d\t: %s\n", fi, argv[fi + 1]);
+			fi++;
+		}
+		printf("-1\t: Exit\n");
+	}
+	i = 0;
+	while (i < argc - 1)
+	{
+		close(fds[i]);
+		printf("closing :\t%s\n",argv[i + 1]);
+		i++;
 	}
 	free(fds);
 	free(line);
