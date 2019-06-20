8bb3e87dec6c5e6efd1e672f66b62f5ec124c72a
Vaughan SCOTT <vscott@c5r2s2.wethinkcode.co.za>
2019-06-20 15:47:37 +0200

    strlen and strchr needed NULL protection

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: vscott <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/23 08:09:56 by vscott            #+#    #+#             */
-/*   Updated: 2019/05/28 11:58:26 by vscott           ###   ########.fr       */
+/*   Updated: 2019/06/20 15:40:48 by vscott           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ char	*ft_strchr(const char *s, int c)
 {
 	const char cc = (char)c;
 
+	if (!s)
+		return (NULL);
 	while (*s && *s != cc)
 		++s;
 	if (*s == cc)
